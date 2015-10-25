7e6e3c453bd51aafa429c644aaa25ed95b708d46
Abraham Arce <xe1gyq@gmail.com>
2015-10-25 00:42:20 +0000

    Modules: mSystem Base Code
    Base code for mSystem
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- 2015/main.py
+++ 2015/main.py
@@ -9,12 +9,12 @@ from core.xtweet import xtweet
 
 from modules.mphraserecognition import mPhraseRecognition
 from modules.mselfie import mSelfie
+from modules.msystem import mSystem
 from modules.mtemperature import mTemperature
 from modules.mweather import mWeather
 from modules.mwolfram import mWolfram
 
 #from modules.mcolors import mColors
-#from modules.mvoice import mVoice
 
 if __name__ == '__main__':
 
@@ -23,34 +23,39 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--modules', help='Project Mode')
     args = parser.parse_args()
 
-    if args.modules == 'mweather':
-        print 'Hello Module mWeather'
-        mweather = mWeather()
-        mweather.report()
-
-    if args.modules == 'mtemperature':
-        print 'Hello Module mTemperature'
-        mtemperature =mTemperature()
-        mtemperature.graph()
-
-    if args.modules == 'mwolfram':
-        print 'Hello Module mWolfram'
-        mwolfram = mWolfram()
-        mwolfram.ask()
+    if args.modules == 'mphraserecognition':
+        print 'Hello Module mPhraseRecognition'
+        mpr = mPhraseRecognition()
+        mpr.decode()
 
     if args.modules == 'mselfie':
         print 'Hello Module mSelfie'
         mselfie = mSelfie()
         mselfie.share()
 
+    if args.modules == 'msystem':
+        print 'Hello Module mSystem'
+        msystem = mSystem()
+        msystem.graph()
+
+    if args.modules == 'mtemperature':
+        print 'Hello Module mTemperature'
+        mtemperature =mTemperature()
+        mtemperature.graph()
+
+    if args.modules == 'mweather':
+        print 'Hello Module mWeather'
+        mweather = mWeather()
+        mweather.report()
+
+    if args.modules == 'mwolfram':
+        print 'Hello Module mWolfram'
+        mwolfram = mWolfram()
+        mwolfram.ask()
+
     if args.modules == 'mcolors':
         print 'Hello Module mColors'
         mcolors = mColors()
         mcolors.show()
-
-    if args.modules == 'mphraserecognition':
-        print 'Hello Module mPhraseRecognition'
-        mpr = mPhraseRecognition()
-        mpr.decode()
 
 # End of File
---
+++ 2015/modules/msystem.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import ConfigParser
+import time
+import psutil
+import random
+import plotly.plotly as py
+from plotly.graph_objs import Scatter, Layout, Figure, Data, Stream, YAxis
+
+class mSystem(object):
+
+    def __init__(self):
+
+        self.configuration = ConfigParser.ConfigParser()
+        self.configuration.read('configuration/credentials.config')
+        self.username = self.configuration.get('plotly','username')
+        self.apikey = self.configuration.get('plotly','apikey')
+        self.streamtokentx = self.configuration.get('plotly','streamtokentx')
+        self.streamtokenrx = self.configuration.get('plotly','streamtokenrx')
+
+        py.sign_in(self.username, self.apikey)
+
+    def graph(self):
+
+        trace_network_tx = Scatter(
+            x=[],
+            y=[],
+            stream=Stream(
+                token=self.streamtokentx,
+            ),
+            yaxis='tx'
+        )
+
+        trace_network_rx = Scatter(
+            x=[],
+            y=[],
+            stream=Stream(
+                token=self.streamtokenrx,
+            ),
+            yaxis='rx'
+        )
+
+        layout = Layout(
+            title='IoTPy Network Health System',
+            yaxis=YAxis(
+                title='Bytes'
+            ),
+            yaxis2=YAxis(
+                title='%',
+                side='right',
+                overlaying="y"
+            )
+        )
+
+        data = Data([trace_network_tx, trace_network_rx])
+        fig = Figure(data=data, layout=layout)
+
+        print py.plot(fig, filename='IoTPy Network Health System', auto_open=False)
+
+        stream_network_tx = py.Stream(self.streamtokentx)
+        stream_network_tx.open()
+
+        stream_network_rx = py.Stream(self.streamtokenrx)
+        stream_network_rx.open()
+        time.sleep(5)
+
+        counter = 0
+
+        while True:
+            output = psutil.net_io_counters()
+            print "Network Bytes Tx %s" % output.bytes_sent
+            print "Network Bytes Rx %s" % output.bytes_recv
+            stream_network_tx.write({'x': counter, 'y': output.bytes_sent })
+            stream_network_rx.write({'x': counter, 'y': output.bytes_recv })
+            counter += 1
+            time.sleep(0.25)
+
+        stream_network_tx.close()
+        stream_network_rx.close()
+
+# End of File
