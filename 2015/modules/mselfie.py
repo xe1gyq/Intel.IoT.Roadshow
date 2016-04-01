7fffd436ec70461a6f5daa2ce328abd2d6484ddd
Abraham Arce <xe1gyq@gmail.com>
2016-04-01 04:30:41 +0000

    Temp Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- 2015/modules/mselfie.py
+++ 2015/modules/mselfie.py
@@ -9,11 +9,12 @@ class mSelfie(object):
 
     def __init__(self):
         self.xc = xCamera()
+        self.tw = xTwitter()
 
     def share(self):
         self.xc.capture()
         picture = self.xc.filepath()
         id = str(randint(0,99))
-        tweet("0x" + id + " #IoT #IoTLearningInit #IoTLearningInitiative IoTPy Selfie Project!", picture)
+        self.tw.tweet("0x" + id + " #IoT #IoTLearningInit #IoTLearningInitiative IoTPy Selfie Project!", picture)
 
 # End of File
